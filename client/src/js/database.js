d7d3967157374ece7d1b82c249f92f71333f6d9b
Jon Beck <jonnydbeck@gmail.com>
2023-01-09 20:41:48 -0500

    Database working

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -10,12 +10,42 @@ const initdb = async () =>
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
-  });
+});
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+//Completely honestly, I'm still getting the errors that were here, like the ones manually added.
+//I'm guessing they are ghost errors that have stayed due to the cache or something i dunno.
+//I'm just hope this actually works everywhere else than on my system then
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+
+  const contactDb = await openDB('contact', 1);
+
+  const tx = contactDb.transaction('contact', 'readwrite');
+
+  const store = tx.objectStore('contact');
+
+  const request = store.add(content);
+
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
+
+
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  const contactDb = await openDB('contact', 1);
+
+  const tx = contactDb.transaction('contact', 'readonly');
+
+  const store = tx.objectStore('contact');
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
